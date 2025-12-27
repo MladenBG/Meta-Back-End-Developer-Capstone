03e8a1f30ac8b439fc8b32012ef19386ddcd9061
MladenBG <mladennnbg1@hotmail.com>
2025-12-27 18:20:39 +0100

    Register Menu and Booking models in admin
    Added admin interfaces for Menu and Booking models with list display, search, and filtering options.

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,27 @@
-from django.contrib import admin
+
 
 # Register your models here.
+
+# Register your models here.
+from django.contrib import admin
+from .models import Menu, Booking
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    # Fields to display in the list view
+    list_display = ('title', 'price', 'inventory')
+    # Add search functionality
+    search_fields = ('title',)
+    # Add filters in the sidebar
+    list_filter = ('price', 'inventory')
+    # Order results by title
+    ordering = ('title',)
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'no_of_guests', 'bookingDate')
+    search_fields = ('name',)
+    list_filter = ('bookingDate', 'no_of_guests')
+    ordering = ('bookingDate',)
